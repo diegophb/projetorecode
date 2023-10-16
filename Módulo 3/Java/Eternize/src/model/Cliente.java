4e3f1e58bd53e9aa4d31c502a3b570b063182215
diegophb <prdiegopi@gmail.com>
2023-10-15 22:45:33 -0300

    Create Cliente.java

---
+++ Módulo 3/Java/Eternize/src/model/Cliente.java
@@ -0,0 +1,64 @@
+package model;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	public Cliente() {
+	}
+
+	public Cliente(int id, String nome, String telefone, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public Cliente(String nome, String telefone, String email) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", telefone=" + telefone + ", email=" + email + "]";
+	}
+
+	
+}
